b5f3f51bda81e0373845a97a6c9242ab19574861
dwara-a <andhara@students.amikom.ac.id>
2026-07-30 10:53:37 +0700

    fix: also redirect admin unauthenticated users to admin login page

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -24,6 +24,11 @@ return Application::configure(basePath: dirname(__DIR__))
             if ($request->is('organization/*')) {
                 return route('organization.login');
             }
+
+            if ($request->is('admin/*')) {
+                return route('admin.login');
+            }
+
             return route('login');
         });
     })
